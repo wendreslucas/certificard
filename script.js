2242ca59729c7633c6103908e0dbb4887ff9544f
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2021-09-24 15:18:19 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+function mudaTema() {
+    document.body.classList.toggle("dark") // tag do html que engloba tudo, vamos pegar o body e atribuir uma classe que vai lá no css e adiciona pra essa classe, darkmode por exemplo e é o js que vai definir quando essa classe vai estar ativa ou não. vai ser no onclick
+}
